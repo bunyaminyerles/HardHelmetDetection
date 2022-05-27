ee10ee02dd32535f7a70b48ab98f754191030f60
bunyaminyerles <57174830+bunyaminyerles@users.noreply.github.com>
2022-05-28 00:57:23 +0300

    first commit

---
+++ detect.py
@@ -0,0 +1,186 @@
+import argparse
+import os
+import sys
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+from flask import Flask, render_template, Response
+from urllib3 import HTTPResponse
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  
+
+from models.common import DetectMultiBackend
+from utils.dataloaders import LoadStreams
+from utils.general import (LOGGER, check_img_size, check_requirements, cv2,
+                           non_max_suppression, print_args, scale_coords)
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
+from playsound import playsound
+import threading
+
+startStop = ''
+
+
+app = Flask(__name__)
+@torch.no_grad()
+def run(
+        weights=ROOT / 'helmet.pt',  # model.pt path(s)
+        source=ROOT / '0',  # file/dir/URL/glob, 0 for webcam
+        data=ROOT / 'data.yaml',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+):
+    thread = threading.Thread(target=playsound, args=('alarm.wav',), daemon=True)
+
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
+    stride, names, pt = model.stride, model.names, model.pt
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Dataloader
+    cudnn.benchmark = True  # set True to speed up constant image size inference
+    dataset = LoadStreams(str(source), img_size=imgsz, stride=stride, auto=pt)
+    bs = len(dataset)  # batch_size
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt else bs, 3, *imgsz))  # warmup
+    dt = [0.0, 0.0, 0.0]
+    for path, im, im0s, vid_cap, s in dataset:
+        t1 = time_sync()
+        im = torch.from_numpy(im).to(device)
+        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        t2 = time_sync()
+        dt[0] += t2 - t1
+
+        # Inference
+        pred = model(im, augment=augment, visualize=visualize)
+        t3 = time_sync()
+        dt[1] += t3 - t2
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        dt[2] += time_sync() - t3
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            im0, frame =  im0s[i].copy(), dataset.count
+            s += f'{i}: '
+            s += '%gx%g ' % im.shape[2:]  # print string
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    c = int(cls)  # integer class
+                    if c == 0 :
+                        if not thread.is_alive():
+                            thread = threading.Thread(target=playsound, args=('alarm.mp3',), daemon=True)
+                            thread.start()
+                    label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                    annotator.box_label(xyxy, label, color=colors(c, True))
+
+            # Stream results
+            im0 = annotator.result()
+            ret, buffer = cv2.imencode('.jpg',im0)
+            frame = buffer.tobytes()
+            global startStop
+            if startStop == 'stop':
+                dataset.updateThread = False
+                yield None
+            else:
+                yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+
+
+        # Print time (inference-only)
+        LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+        
+            
+        
+
+
+
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model path(s)')
+    parser.add_argument('--source', type=str, default=ROOT / '0', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
+    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[416], help='inference size h,w')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
+    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='show results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--visualize', action='store_true', help='visualize features')
+    parser.add_argument('--project', default=ROOT / 'runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
+    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    opt = parser.parse_args()
+    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
+    print_args(vars(opt))
+    return opt
+
+@app.route('/video_feed')
+def video_feed():
+    global startStop
+    startStop = 'start'
+    check_requirements(exclude=('tensorboard', 'thop'))
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(run(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/stop')
+def stop():
+    global startStop
+    startStop = 'stop'
+    return 'Stopped'
+
+@app.route('/')
+def index():
+    global startStop
+    startStop = 'stop'
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run()
